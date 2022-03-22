b1c3207bc2d0ceb784b0831234decd3b62542daf
Dylan <user@users-MBP-2.broadband>
2022-03-22 23:55:24 +0000

    Updating schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,13 @@ CREATE TABLE departments(
     department VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE roles(
+    id INT(11) AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(12,2) NOT NULL,
+    department_ID INT,
+    FOREIGN KEY (department_ID)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
