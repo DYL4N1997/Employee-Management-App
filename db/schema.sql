08bb86129fa3f95769ebbce6e58f30c0747809b1
Dylan <user@users-MBP-2.broadband>
2022-03-22 23:47:49 +0000

    Updating schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 USE employees_db;
+
+CREATE TABLE departments(
+    id INT(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    department VARCHAR(30) NOT NULL
+);
+
