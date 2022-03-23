a835645eba055d7fde6277d035d3b052886a53e4
Dylan <user@users-MBP-2.broadband>
2022-03-23 23:28:18 +0000

    Updating db seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,4 +17,11 @@ VALUES ("Sales Lead", "100000", 1),
        ("Lawyer", "190000", 4);
 
 INSERT INTO employees (first_name, last_name, role_ID, manager_ID)
-VALUES 
+VALUES ("John", "Doe", 1, NULL),
+       ("Mike", "Chan", 2, 1),
+       ("Ashley", "Rodriguez", 3, NULL),
+       ("Kevin", "Tupik", 4, 3),
+       ("Kunal", "Singh", 5, NULL),
+       ("Malia", "Brown", 6, 5),
+       ("Sarah", "Lourd", 7, NULL),
+       ("Tom", "Allen", 8, 7);
