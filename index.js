2f9452b8541e57d12d275c17aea212ffa57a3da1
Dylan <user@users-MBP-2.broadband>
2022-03-24 23:50:53 +0000

    Updating index.js logic

--- index.js
+++ index.js
@@ -31,5 +31,13 @@ const showAllRoles = () => {
     const sql = `SELECT roles_id AS id, roles.title AS title, department.name AS department, 
     roles.salary AS salary FROM roles JOIN department ON roles.department_id = department.id;`;
 
-    
+    db.query (sql, (err, rows) => {
+        if (err) {
+            console.log(err.message);
+            return;
+        } else {
+            terminal.table(rows);
+        init();
+        }
+    })
 }
