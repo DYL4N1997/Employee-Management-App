85dbbe5e25ac9a10e5781189dcc80ed16073bf68
DYL4N1997 <82978161+DYL4N1997@users.noreply.github.com>
2022-03-28 00:01:42 +0100

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -32,28 +32,3 @@ CREATE TABLE employees (
     REFERENCES roles(id)
     ON DELETE SET NULL
 );
-
-
-
--- CREATE TABLE department (
---     id INT(11) AUTO_INCREMENT NOT NULL,
---     name VARCHAR(30) NOT NULL,
---     PRIMARY KEY(id)
--- );
-
--- CREATE TABLE roles (
---     id INT(11) AUTO_INCREMENT NOT NULL,
---     title VARCHAR(30) NOT NULL,
---     salary DECIMAL(12,2) NOT NULL,
---     department_id INT NOT NULL,
---     PRIMARY KEY (id)
--- );
-
--- CREATE TABLE employees (
---     id INT(11) NOT NULL AUTO_INCREMENT,
---     first_name VARCHAR(30) NOT NULL,
---     last_name VARCHAR(30) NOT NULL,
---     role_id INT(11) NOT NULL,
---     manager_id INT(11),
---     PRIMARY KEY(id)
--- );
